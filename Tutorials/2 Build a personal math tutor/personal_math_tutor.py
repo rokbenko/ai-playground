e19d8b2dea7ad9c983005ed2b0e728a32e2de0b6
Rok Benko <ksjeno@gmail.com>
2023-12-19 14:24:12 +0100

    Edit personal_math_tutor.py
    Fix to handle queued status too

--- Tutorials/2 Build a personal math tutor/personal_math_tutor.py
+++ Tutorials/2 Build a personal math tutor/personal_math_tutor.py
@@ -33,7 +33,7 @@ my_run = client.beta.threads.runs.create(
 print(f"This is the run object: {my_run} \n")
 
 # Step 5: Periodically retrieve the Run to check on its status to see if it has moved to completed
-while my_run.status == "in_progress":
+while my_run.status in ["queued", "in_progress"]:
     keep_retrieving_run = client.beta.threads.runs.retrieve(
         thread_id=my_thread.id,
         run_id=my_run.id
@@ -42,17 +42,20 @@ while my_run.status == "in_progress":
 
     if keep_retrieving_run.status == "completed":
         print("\n")
+
+        # Step 6: Retrieve the Messages added by the Assistant to the Thread
+        all_messages = client.beta.threads.messages.list(
+            thread_id=my_thread.id
+        )
+
+        print("------------------------------------------------------------ \n")
+
+        print(f"User: {my_thread_message.content[0].text.value}")
+        print(f"Assistant: {all_messages.data[0].content[0].text.value}")
+
         break
+    elif keep_retrieving_run.status == "queued" or keep_retrieving_run.status == "in_progress":
+        pass
     else:
         print(f"Run status: {keep_retrieving_run.status}")
         break
-
-# Step 6: Retrieve the Messages added by the Assistant to the Thread
-all_messages = client.beta.threads.messages.list(
-  thread_id=my_thread.id
-)
-
-print("------------------------------------------------------------ \n")
-
-print(f"User: {my_thread_message.content[0].text.value}")
-print(f"Assistant: {all_messages.data[0].content[0].text.value}")
